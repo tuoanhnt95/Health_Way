c667f469c9ed0463295bb1f8234575ef1a70f938
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-22 19:05:05 +0900

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :set_ups, only: %i[index show new create] do
+    resources :health_checks, only: %i[new create]
+  end
+
+  resources :health_checks, only: %I[index show edit update]
 end
