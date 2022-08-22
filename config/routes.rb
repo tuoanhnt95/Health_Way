adaac43dd7e7149795e79318b89fdc660d0986cd
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-22 19:48:07 +0900

    created controllers for setup and health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'health_checks/index'
+  get 'health_checks/show'
+  get 'health_checks/new'
+  get 'health_checks/edit'
+  get 'set_ups/index'
+  get 'set_ups/show'
   devise_for :users
   root to: "pages#home"
 
@@ -6,5 +12,5 @@ Rails.application.routes.draw do
     resources :health_checks, only: %i[new create]
   end
 
-  resources :health_checks, only: %I[index show edit update]
+  resources :health_checks, only: %i[index show edit update]
 end
