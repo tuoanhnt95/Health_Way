d3a742082de7759da041a8da95e86d4a732a9abe
mengrui song <summerrock1017@gmail.com>
2022-08-23 22:31:44 +0900

    set up new and create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
   belongs_to :company
-  belongs_to :set_up
   has_many :health_checks, dependent: :destroy
   has_one_attached :photo
 end
