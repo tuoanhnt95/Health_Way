fbd76a25633afc47289b9188ee65e8d1bc097876
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-24 18:13:35 +0900

    create range date picker

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import LitepickerController from "./litepicker_controller"
+application.register("litepicker", LitepickerController)
+
 import MapController from "./map_controller"
 application.register("map", MapController)
