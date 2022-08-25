4123dca2a67eb2a9754921b064989b011cebf7e9
mengrui song <summerrock1017@gmail.com>
2022-08-25 15:31:32 +0900

    seperate the progress bar as two

--- app/controllers/set_ups_controller.rb
+++ app/controllers/set_ups_controller.rb
@@ -7,7 +7,14 @@ class SetUpsController < ApplicationController
 
   def show
     authorize @set_up
-    @complete_rate = ((@set_up.health_checks.count) / (@set_up.company.users.count).to_f * 100).ceil
+    @appointment_rate = ((@set_up.health_checks.count) / (@set_up.company.users.count).to_f * 100).ceil
+    @complete = 0
+    @set_up.health_checks.each do |health_check|
+      if health_check.result.attached?
+        @complete += 1
+      end
+    end
+    @complete_rate = (@complete / (@set_up.company.users.count).to_f * 100).ceil
   end
 
   def new
