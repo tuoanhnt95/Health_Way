adfe77815e20434abdafb9ea0d1da52f5ba40730
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-30 09:46:00 +0900

    Order the table to display employees by last name alphabetically, not display No appointment

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module HealthWay
-  class Application < Rails::Applicationpull 
+  class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
       generate.helper false
