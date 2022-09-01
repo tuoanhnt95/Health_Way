7e60e0d660dd2bf3106b3e69ae603a1c982ded61
Oanh Nguyen <myspamvn@yahoo.com>
2022-09-02 01:14:07 +0900

    fix meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
-
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
@@ -13,10 +12,10 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
   end
-  
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
+
+  # def default_url_options
+  #   { host: ENV["DOMAIN"] || "localhost:3000" }
+  # end
 
   private
 
